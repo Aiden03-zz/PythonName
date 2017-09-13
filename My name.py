00aaf2a50aaf9916c9b7c8bc63f108ed6538f8c1
Aiden <31713909+Aiden03@users.noreply.github.com>
2017-09-12 20:56:53 -0400

    Add files via upload

---
+++ My name.py
@@ -0,0 +1,19 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> name = input("What is your name?")
+What is your name?Aiden
+>>> for x in range(100):
+	print(name, end = " ")
+
+	
+Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden Aiden 
+>>>  name = input("What is your name?")
+ 
+SyntaxError: unexpected indent
+>>> name = input("What is your name?")
+What is your name?Aiden
+>>> print(name, /n = " rules!")
+SyntaxError: invalid syntax
+>>> print(name, end = " rules!")
+Aiden rules!
+>>> 
